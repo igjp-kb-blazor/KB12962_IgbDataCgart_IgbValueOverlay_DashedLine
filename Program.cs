5e84fe91f5d781e6d1511c12a6fedffda95982df
Tomoya Washio <TWashio@infragistics.com>
2024-02-20 14:17:17 +0900

    初回コミット

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using IgniteUI.Blazor.Controls;
+using KB12962_IgbDataCgart_IgbValueOverlay_DashedLine;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddIgniteUIBlazor(
+        typeof(IgbLegendModule),
+    typeof(IgbDataChartCoreModule),
+    typeof(IgbDataChartCategoryModule),
+    typeof(IgbDataChartInteractivityModule),
+    typeof(IgbDataChartAnnotationModule),
+    typeof(IgbDataChartCategoryCoreModule),
+    typeof(IgbAnnotationLayerProxyModule),
+    typeof(IgbCalloutLayerModule),
+    typeof(IgbValueOverlayModule)
+    );
+
+await builder.Build().RunAsync();
